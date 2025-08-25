b9af6f125893bfe360eb48ee1ce197e01ed5459c
Fedgy <fedgysainte@gmail.com>
2025-08-24 20:29:09 -0400

    Chapter_02

---
+++ Chapter_04/magicians.py
@@ -0,0 +1,11 @@
+magicians = ['alice', 'Bob', 'Carolina']
+for magician in magicians:
+    print(magician)
+
+
+magicians = ['alice', 'Bob', 'Carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a good trick!\n")
+    print(f"I can't wait to see your next trick {magician.title()}.\n")
+
+print("Thank you, everyone. That was a great magic show!")
---
+++ Chapter_04/pizzas.py
@@ -0,0 +1,13 @@
+pizzas = ['meatlovers', 'cheese', 'chicken bacon ranch']
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza!")
+
+print("I really love pizza!\n")
+
+
+animals = ['cat', 'dog', 'rabbit']
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet!")
+
+print("Any of these animals would make a great pet!")
+
